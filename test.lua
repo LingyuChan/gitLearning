32f35c5a91c052916641a12a2c18b79083f8794f
LisaChan <LisaChen@boyaa.com>
2017-04-11 15:06:51 +0800

    add

--- test.lua
+++ test.lua
@@ -3,4 +3,8 @@
 print("hello world")
 print("hello, Big stone")
 print("test.lua")
-print("测试：已经commit操作之后执行checkout -- filename做的修改，我还在不？")
+print("测试：已经commit操作之后执行checkout -- filename做的修改，我还在不？")  -- 还真的可以耶
+
+print("测试放到暂存区的内容撤回到工作区")
+
+print("提交到工作区")
